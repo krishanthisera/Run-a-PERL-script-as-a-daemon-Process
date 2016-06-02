1ae1aaa133740b8bda6eed00d27b68f564d714ce
Krishan Thisera <krishees@gmail.com>
2016-06-02 08:35:32 +0000

    Add files via upload

---
+++ logger.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+#==================================
+#Only If you run this using crontab 
+#----------------------------------
+use lib '/root/perl5/lib/perl5'; 
+use Net::Finger;		 	
+#==================================
+
+use Proc::Daemon;
+Proc::Daemon::Init;
+
+my $running = 1;
+while ($running) {
+
+#...................
+#Your programe here!!
+#...................
+
+}
